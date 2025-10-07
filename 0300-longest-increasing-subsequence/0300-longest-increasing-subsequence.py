d3d8d0e2a389882175cd5009292e67eafb58bb00
Ambuj Vashistha <ambujva123@gmail.com>
2025-10-07 13:21:20 +0530

    Time: 857 ms (70.73%), Space: 18 MB (80.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,21 +1,35 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # memo={}
-        n = len(nums)
-        l=[[-1]*n for i in range(len(nums))]
-        def helper(index,prev_index):
-            if index==len(nums):
-                return 0
+
+        # n = len(nums)
+        # l=[[-1]*n for i in range(len(nums))]
+        # def helper(index,prev_index):
+        #     if index==len(nums):
+        #         return 0
             
-            if l[index][prev_index] != -1:
-                return l[index][prev_index]
+        #     if l[index][prev_index] != -1:
+        #         return l[index][prev_index]
 
-            not_take=helper(index+1,prev_index)
-            take=0
+        #     not_take=helper(index+1,prev_index)
+        #     take=0
 
-            if prev_index==-1 or nums[index]>nums[prev_index] :
-                take= 1+ helper(index+1,index)
-            l[index][prev_index]=max(take,not_take)
-            return l[index][prev_index]
-        return helper(0,-1)
+        #     if prev_index==-1 or nums[index]>nums[prev_index] :
+        #         take= 1+ helper(index+1,index)
+        #     l[index][prev_index]=max(take,not_take)
+        #     return l[index][prev_index]
+        # return helper(0,-1)
+
+        arr=[-1 for _ in range(len(nums))]
+        # print(arr)
+        for i in range(len(nums)):
+            if i==0:
+                arr[i]=1
+            maxi=0
+            for j in range(i):
+                if nums[j]<nums[i]:
+                    if arr[j]>maxi:
+                        maxi=arr[j]
+            arr[i]=maxi+1
+        # print(arr)
+        return max(arr)
         
