5124f01d97cf9867f4393c339e5914d3933d65e0
Ambuj Vashistha <ambujva123@gmail.com>
2025-10-11 19:39:44 +0530

    Time: 371 ms (47.3%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,3 +1,9 @@
 # Write your MySQL query statement below
+with newTable as (
+    select email,count(email) 
+    from person 
+    group by email 
+    having count(email) >= 2
+    )
 select email 
-from (select email,count(email) from person group by email having count(email) >= 2) et;
+from  newtable;
