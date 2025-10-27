10e0ad36dc6c5a97578297e45c6bd5848bc644a9
Ambuj Vashistha <ambujva123@gmail.com>
2025-10-27 15:56:00 +0530

    Time: 840 ms (70.85%), Space: 18.1 MB (54.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,35 +1,12 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-
-        # n = len(nums)
-        # l=[[-1]*n for i in range(len(nums))]
-        # def helper(index,prev_index):
-        #     if index==len(nums):
-        #         return 0
-            
-        #     if l[index][prev_index] != -1:
-        #         return l[index][prev_index]
-
-        #     not_take=helper(index+1,prev_index)
-        #     take=0
-
-        #     if prev_index==-1 or nums[index]>nums[prev_index] :
-        #         take= 1+ helper(index+1,index)
-        #     l[index][prev_index]=max(take,not_take)
-        #     return l[index][prev_index]
-        # return helper(0,-1)
-
-        arr=[-1 for _ in range(len(nums))]
-        # print(arr)
-        for i in range(len(nums)):
-            if i==0:
-                arr[i]=1
+        lis=[-1 for _ in range(len(nums))]
+        lis[0]=1
+        for i in range(1,len(nums)):
             maxi=0
-            for j in range(i):
+            for j in range(i+1):
                 if nums[j]<nums[i]:
-                    if arr[j]>maxi:
-                        maxi=arr[j]
-            arr[i]=maxi+1
-        # print(arr)
-        return max(arr)
-        
+                    if lis[j]>maxi:
+                        maxi=lis[j]
+            lis[i]=maxi+1
+        return max(lis)
