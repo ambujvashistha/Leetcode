b5abeefc5d473f5d264f49e5ed69615bff4bfa72
Ambuj Vashistha <ambujva123@gmail.com>
2025-11-27 12:33:57 +0530

    Time: 2288 ms (5.01%), Space: 543 MB (6.07%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,33 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
-        buy=[0 for _ in range(len(prices))]
-        sell=[0 for _ in range(len(prices))]
-        mini=prices[0]
-        for i in range(len(prices)):
-            if prices[i]-mini>0:
-                sell[i]=prices[i]-mini
-            
-            else:
-                mini=prices[i]
-        maxi=prices[-1]
-        for i in range(len(prices)-1,-1,-1):
-            if prices[i]-maxi<0:
-                buy[i]=maxi-prices[i]
-            else:
-                maxi=prices[i]
+        memo={}
+        def helper(i,flag,count):
+            if i==len(prices):
+                return 0
+            if (i,flag,count) in memo:
+                return memo[(i,flag,count)]
 
-        # print(sell,buy)
+            ans=helper(i+1,flag,count)
 
-        newmaxi=sell[0]
-        profit=0
-        for i in range(1,len(prices)):
-            # print(profit)
-            # x=sell[:i+1]
-            if sell[i]>=newmaxi:
-                newmaxi=sell[i]
-            if buy[i]+newmaxi>profit:
-                # print(i,maxi)
-                profit=buy[i]+newmaxi
-        
-        return profit
+            if flag and count>0:
+                ans=max(ans,helper(i+1,not(flag),count-1)-prices[i])
+            elif not(flag):
+                ans=max(ans,helper(i+1,not(flag),count)+prices[i])
+            memo[(i,flag,count)]=ans
+            return memo[(i,flag,count)]
+        return helper(0,True,2)
+
